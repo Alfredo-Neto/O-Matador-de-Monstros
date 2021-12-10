c9742b8691871d3bd445593fa7e03bf78f1199eb
Alfredo Neto <alfredoneto934@gmail.com>
2021-12-10 10:51:47 -0300

    implemented giveUpGame and battle functions

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -2,10 +2,21 @@ new Vue({
     el: '#app',
     data: {
         initGame: false,
+        width: '100%',
+        life: 100,
     },
     methods: {
         beginGame() {
             this.initGame = this.initGame ? false : true;
-        }
+        },
+        giveUpGame() {
+            this.initGame = this.initGame ? false : true;
+            this.life = 100;
+            this.width = `${this.life}%`;
+        },
+        battle() {
+            this.life -= 10;
+            this.width = `${this.life}%`;
+        },
     },
 })
