39c8fa6a9193c6a36f135882d071f2575db70003
Alfredo Neto <alfredoneto934@gmail.com>
2021-12-10 12:51:44 -0300

    implemented playerAttack and monsterAttack

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -3,20 +3,34 @@ new Vue({
     data: {
         initGame: false,
         width: '100%',
-        life: 100,
+        playerLife: 100,
+        monsterLife: 100,
+    },
+    computed: {
+        
     },
     methods: {
-        beginGame() {
+        game() {
             this.initGame = this.initGame ? false : true;
-        },
-        giveUpGame() {
-            this.initGame = this.initGame ? false : true;
-            this.life = 100;
-            this.width = `${this.life}%`;
+            this.resetLife();
         },
         battle() {
-            this.life -= 10;
-            this.width = `${this.life}%`;
+            this.playerAttack();
+            this.monsterAttack();
         },
+        playerAttack(){
+            this.monsterLife -= 10;
+            this.width = `${this.monsterLife}%`;
+        },
+        monsterAttack(){
+            this.playerLife -= 10;
+            this.width = `${this.playerLife}%`;
+        },
+        resetLife() {
+            this.playerLife = 100;
+            this.width = `${this.playerLife}%`;
+            this.monsterLife = 100;
+            this.width = `${this.monsterLife}%`;
+        }
     },
 })
