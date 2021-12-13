6a1160ec1ddbfa090cc2352b2f023171db32817e
Alfredo Neto <alfredoneto934@gmail.com>
2021-12-13 12:37:05 -0300

    fixed attack

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,36 +1,42 @@
 new Vue({
     el: '#app',
     data: {
-        initGame: false,
-        width: '100%',
+        isRunning: false,
         playerLife: 100,
         monsterLife: 100,
+        playerDamage: 16,
+        monsterDamage: 15,
+        special: 50,
+        maxLife: 100,
     },
     computed: {
-        
+        hasResult(){
+           return this.playerLife == 0 || this.monsterLife == 0;
+        },
     },
     methods: {
         game() {
-            this.initGame = this.initGame ? false : true;
+            this.isRunning = this.isRunning ? false : true;
             this.resetLife();
         },
-        battle() {
-            this.playerAttack();
-            this.monsterAttack();
+        attack(special){
+            this.hurt('playerLife', 'monsterDamage', false);
+            this.hurt('monsterLife', 'playerDamage', special);
         },
-        playerAttack(){
-            this.monsterLife -= 10;
-            this.width = `${this.monsterLife}%`;
+        hurt(prop, whichDamage, special){
+            const hurt = special ? this.special : this[whichDamage];
+            this[prop] -= hurt;
+            this[prop] = this[prop] < 0 ? 0 : this[prop];
+            if (this[prop] == 0) {
+                this.isRunning = false;
+            }
         },
-        monsterAttack(){
-            this.playerLife -= 10;
-            this.width = `${this.playerLife}%`;
+        getRandomArbitrary(min, max) {
+            return Math.random() * (max - min) + min;
         },
         resetLife() {
-            this.playerLife = 100;
-            this.width = `${this.playerLife}%`;
-            this.monsterLife = 100;
-            this.width = `${this.monsterLife}%`;
+            this.playerLife = this.maxLife;
+            this.monsterLife = this.maxLife;
         }
     },
 })
